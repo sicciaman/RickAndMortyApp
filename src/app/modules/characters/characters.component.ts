7bc3124ceff417abf120767939037d132af7145b
a.secci96 <a.secci@stamtech.com>
2022-07-24 22:49:59 +0200

    added favorite section, style refactor

--- src/app/modules/characters/characters.component.ts
+++ src/app/modules/characters/characters.component.ts
@@ -1,37 +1,93 @@
 import {Component, OnInit} from '@angular/core';
+import {FormControl} from '@angular/forms';
+import {debounceTime, tap} from 'rxjs';
 import {CharactersService} from './characters.service';
 import {Character} from './interfaces/character.interface';
 import {Pagination} from '../../models/pagination';
+import {FavoriteAction} from './components/character-card/character-card.component';
 
 @Component({
-  selector: 'as-characters',
-  templateUrl: './characters.component.html',
-  styleUrls: ['./characters.component.scss']
+    selector: 'as-characters',
+    templateUrl: './characters.component.html',
+    styleUrls: ['./characters.component.scss']
 })
 export class CharactersComponent implements OnInit {
-  characters: Character[];
-  pagination: Pagination;
+    // Search form control input
+    characterInput: FormControl;
 
-  constructor(private charactersService: CharactersService) {
-    this.characters = [];
-    this.pagination = new Pagination();
-  }
+    // Query string for character name search
+    query: string;
 
-  ngOnInit(): void {
-    this.fetchCharacters();
-  }
+    // List of characters
+    characters: Character[];
 
-  fetchCharacters(): void {
-    this.charactersService.findAll(this.pagination).subscribe((data) => {
-      this.characters = data.results;
-      this.pagination = {page: this.pagination.page, ...data.info};
-    });
-  }
+    // List of favorite characters IDs
+    favorites: Set<number>;
 
-  changePage(action: 'prev' | 'next'): void {
-    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
-    action === 'next' ? this.pagination.page++ : this.pagination.page--;
-    this.fetchCharacters();
-  }
+    // Pagination info
+    pagination: Pagination;
 
+    constructor(private charactersService: CharactersService) {
+        this.characterInput = new FormControl<string>('');
+        this.query = '';
+        this.characters = [];
+        this.favorites = new Set<number>();
+        this.pagination = new Pagination();
+    }
+
+    ngOnInit(): void {
+        this.fetchCharacters();
+        this.characterInput.valueChanges
+            .pipe(
+                debounceTime(500),
+                tap((query) => {
+                    // Clear pagination before new request
+                    this.pagination.page = 1;
+                    this.query = query;
+                }),
+            )
+            .subscribe(() => {
+                this.fetchCharacters();
+            });
+    }
+
+    /**
+     * Load characters list and pagination info
+     */
+    fetchCharacters(): void {
+        this.charactersService.findAll(this.pagination, this.query).subscribe((data) => {
+            this.characters = data.results;
+            this.pagination = {page: this.pagination.page, ...data.info};
+        });
+    }
+
+    /**
+     * Change pagination page and reload characters list
+     *
+     * @param action Can be next or prev based on the button clicked by the user
+     */
+    changePage(action: 'prev' | 'next'): void {
+        // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+        action === 'next' ? this.pagination.page++ : this.pagination.page--;
+        this.fetchCharacters();
+    }
+
+    /**
+     * Add or remove a character from favourites list
+     *
+     * @param characterID Character ID
+     */
+    toggleFavorite({id, action}: { id: number; action: FavoriteAction }): void {
+        if (action === 'ADD') {
+            if (this.favorites.has(id)) {
+                alert('Character already in favorites');
+                return;
+            }
+            this.favorites.add(id);
+            console.log(`Character with ID ${id} added to favorites`);
+        } else {
+            this.favorites.delete(id);
+            console.log(`Character with ID ${id} removed from favorites`);
+        }
+    }
 }

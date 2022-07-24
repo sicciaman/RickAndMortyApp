904d4afc9e3d7a904a5c2d5d98f5cafb272fc8ca
a.secci96 <a.secci@stamtech.com>
2022-07-24 22:49:24 +0200

    added reactive forms module import

--- src/app/modules/characters/characters.module.ts
+++ src/app/modules/characters/characters.module.ts
@@ -1,19 +1,25 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {ReactiveFormsModule} from '@angular/forms';
 
 import {CharactersRoutingModule} from './characters-routing.module';
 import {CharactersComponent} from './characters.component';
+import {CharacterCardComponent} from './components/character-card/character-card.component';
+import {FindCharacterByIdPipe} from './pipes/find-character-by-id.pipe';
 
 
 @NgModule({
     declarations: [
-        CharactersComponent
+        CharactersComponent,
+        CharacterCardComponent,
+        FindCharacterByIdPipe
     ],
     exports: [
         CharactersComponent
     ],
     imports: [
         CommonModule,
+        ReactiveFormsModule,
         CharactersRoutingModule
     ]
 })
